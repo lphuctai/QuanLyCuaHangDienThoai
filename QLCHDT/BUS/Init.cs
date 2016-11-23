7a6917388574419076ffc135be49bcd3b6469420
Lâm Phúc Tài <lphuctai@live.com>
2016-11-23 17:40:32 +0700

    Init

---
+++ QLCHDT/BUS/Init.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BUS
+{
+	public class Init
+	{
+		private static NhanVien _nhanVien = null;
+
+		public static NhanVien nhanVien
+		{
+			get { return _nhanVien; }
+			set { _nhanVien = value; }
+		}
+	}
+}
